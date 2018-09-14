7e318cdc94bf7b2ccc1c7f7171909d6ba4bc3681
MaSanPi <1029191684@qq.com>
2018-09-14 09:45:45 +0800

    项目初始化

---
+++ BeautyPictures/BeautyPictures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeautyPictures
+//
+//  Created by 马波 on 2018/9/14.
+//  Copyright © 2018年 MB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeautyPictures/BeautyPictures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeautyPictures
+//
+//  Created by 马波 on 2018/9/14.
+//  Copyright © 2018年 MB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
